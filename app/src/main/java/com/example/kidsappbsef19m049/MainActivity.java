391c8d702a6237c247d8e87d84af257b0cc4dd00
Hanzala_Iqbal <hanzalaiqbal313@gmail.com>
2022-05-21 22:00:50 +0500

    Initial Commit! Mail Screen Layout!

---
+++ app/src/main/java/com/example/kidsappbsef19m049/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kidsappbsef19m049;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
