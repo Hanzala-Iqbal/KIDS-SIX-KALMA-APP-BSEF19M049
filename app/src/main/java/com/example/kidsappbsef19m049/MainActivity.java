0722dca55e443b268e5fe3c036278b1652e157f3
Hanzala_Iqbal <hanzalaiqbal313@gmail.com>
2022-05-22 14:05:14 +0500

    Six_Kalmas.java/xml addition + some changes

--- app/src/main/java/com/example/kidsappbsef19m049/MainActivity.java
+++ app/src/main/java/com/example/kidsappbsef19m049/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.kidsappbsef19m049;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button repo, learn, quiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        repo = findViewById(R.id.repobtn);
+        learn = findViewById(R.id.learnbtn);
+        quiz = findViewById(R.id.quizbtn);
+
+        repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "REPO!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeActivity();
+            }
+        });
     }
+        private void changeActivity()
+        {
+            Intent intent = new Intent(this,Six_Kalmas.class);
+            startActivity(intent);
+        }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/kidsappbsef19m049/Six_Kalmas.java
@@ -0,0 +1,14 @@
+package com.example.kidsappbsef19m049;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Six_Kalmas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_six_kalmas);
+    }
+}
