c2ce6710499e5f60876206f70941ad07342a6344
Hanzala_Iqbal <hanzalaiqbal313@gmail.com>
2022-05-22 15:55:01 +0500

    All six Kalmas XML files Added!

---
+++ app/src/main/java/com/example/kidsappbsef19m049/Fifth_Kalma.java
@@ -0,0 +1,14 @@
+package com.example.kidsappbsef19m049;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fifth_Kalma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth_kalma);
+    }
+}
--- app/src/main/java/com/example/kidsappbsef19m049/Six_Kalmas.java
+++ app/src/main/java/com/example/kidsappbsef19m049/Six_Kalmas.java
@@ -33,7 +33,36 @@ public class Six_Kalmas extends AppCompatActivity {
             public void onClick(View v) {
                 changeActivity2();
             }
+
         });
+        third.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeActivity3();
+            }
+
+        });
+        fourth.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        changeActivity4();
+                    }
+
+                });
+        fifth.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        changeActivity5();
+                    }
+
+                });
+        sixth.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        changeActivity6();
+                    }
+
+                });
 
     }
 
@@ -47,6 +76,26 @@ public class Six_Kalmas extends AppCompatActivity {
     private void changeActivity2()
     {
         Intent intent = new Intent(this,Second_Kalma.class);
+        startActivity(intent);
+    }
+    private void changeActivity3()
+    {
+        Intent intent = new Intent(this, Third_Kalma.class);
+        startActivity(intent);
+    }
+     private void changeActivity4()
+    {
+        Intent intent = new Intent(this, Fourth_Kalma.class);
+        startActivity(intent);
+    }
+    private void changeActivity5()
+    {
+        Intent intent = new Intent(this, Fifth_Kalma.class);
+        startActivity(intent);
+    }
+    private void changeActivity6()
+    {
+        Intent intent = new Intent(this, Sixth_Kalma.class);
         startActivity(intent);
     }
 
