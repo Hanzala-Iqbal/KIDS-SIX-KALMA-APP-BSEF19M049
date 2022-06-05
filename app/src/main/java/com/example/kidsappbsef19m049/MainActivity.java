0fb13a0ccb6caa1cd6353c72b6471ee57076b8d6
Hanzala_Iqbal <hanzalaiqbal313@gmail.com>
2022-06-05 15:25:08 +0500

    Main Screen(MainActivity) ListView!

--- app/src/main/java/com/example/kidsappbsef19m049/MainActivity.java
+++ app/src/main/java/com/example/kidsappbsef19m049/MainActivity.java
@@ -6,57 +6,50 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Button repo, learn, quiz;
+    ListView mainScreenList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        repo = findViewById(R.id.repobtn);
-        learn = findViewById(R.id.learnbtn);
-        quiz = findViewById(R.id.quizbtn);
+        mainScreenList = (ListView) findViewById(R.id.ListViewMainScreen);
+        ArrayList<String> MainScreenList = new ArrayList<String>();
+        MainScreenList.add("REPO");
+        MainScreenList.add("LEARN");
+        MainScreenList.add("QUIZ");
 
-        repo.setOnClickListener(new View.OnClickListener() {
+        ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,MainScreenList);
+        mainScreenList.setAdapter(arrayAdapter);
+
+        mainScreenList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                torepo();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Hanzala-Iqbal/KIDS-SIX-KALMA-APP-BSEF19M049/tree/master")));
+                }
+                if (position == 1) {
+                    Intent intent = new Intent(MainActivity.this,Six_Kalmas.class);
+                    startActivity(intent);
+                }
+                if (position == 2) {
+                    Intent intent = new Intent(MainActivity.this,QuizActivity.class);
+                    startActivity(intent);
+                }
+
+
             }
         });
 
-        learn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toLearn();
-            }
-        });
-
-        quiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toquiz();
-            }
-        });
     }
-        private void toLearn()
-        {
-            Intent intent = new Intent(this,Six_Kalmas.class);
-            startActivity(intent);
-        }
-
-        private void torepo()
-        {
-            startActivity(new Intent(Intent.ACTION_VIEW,
-                    Uri.parse("https://github.com/Hanzala-Iqbal/KIDS-SIX-KALMA-APP-BSEF19M049/tree/master")));
-        }
-
-        private void toquiz()
-        {
-            Intent intent = new Intent(this,QuizActivity.class);
-            startActivity(intent);
-        }
 
 }
